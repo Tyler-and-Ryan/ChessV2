3fab953756148dc3df3cf4f5bffa6a4aa31275c2
RyanWatson425 <watsonr3@spu.edu>
2023-04-26 12:43:58 -0700

    init

---
+++ chess-v2/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <header className="App-header">
+        <div>Start</div>
+        <div>Chess Version 2</div>
+        <div>Reset</div>
+      </header>
+      <body className="App-body">
+        <div>
+          Chess Board Here
+        </div>
+      </body>
+      <footer className="App-footer"><p>Developed by <a href="https://linkedin.com/in/ryan-watson-4a8690213" target="_blank">Ryan Watson</a></p></footer>
+    </Fragment>
+  );
+}
+
+export default App;
