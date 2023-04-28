82628c774df0389d0bbbd67d39f7efa7133855f9
RyanWatson425 <watsonr3@spu.edu>
2023-04-27 21:18:31 -0700

    chessboard css complete

--- chess-v2/src/Tile.js
+++ chess-v2/src/Tile.js
@@ -1,13 +1,11 @@
-import './Tile.css'
+import "./Tile.css";
 
 const Tile = (props) => {
-    
-
-    return (
-        <div className="Tile">
-            {props.svg && <img src={props.svg} alt={props.altText} />}
-        </div>
-    );
-}
+  return (
+    <div className="Tile">
+      {(props.altText !== "") && <img src={props.svg} alt={props.altText} />}
+    </div>
+  );
+};
 
 export default Tile;
