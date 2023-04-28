3eb168df99ceb5cf4922aa09b9c0768947cdf5a8
RyanWatson425 <watsonr3@spu.edu>
2023-04-28 00:02:24 -0700

    started implementing board labels
    Also added pseudocode for algorithm

--- chess-v2/src/ChessBoard.js
+++ chess-v2/src/ChessBoard.js
@@ -1,89 +1,170 @@
-import React from 'react'
-import './ChessBoard.css'
-import Tile from './Tile.js'
-import blackKing from './visualAssets/blackKing.svg'
-import whiteKing from './visualAssets/whiteKing.svg'
-import blackBishop from './visualAssets/blackBishop.svg'
-import whiteBishop from './visualAssets/whiteBishop.svg'
-import blackKnight from './visualAssets/blackKnight.svg'
-import whiteKnight from './visualAssets/whiteKnight.svg'
-import blackPawn from './visualAssets/blackPawn.svg'
-import whitePawn from './visualAssets/whitePawn.svg'
-import blackQueen from './visualAssets/blackQueen.svg'
-import whiteQueen from './visualAssets/whiteQueen.svg'
-import blackRook from './visualAssets/blackRook.svg'
-import whiteRook from './visualAssets/whiteRook.svg'
+import React from "react";
+import "./ChessBoard.css";
+import Tile from "./Tile.js";
+import blackKing from "./visualAssets/blackKing.svg";
+import whiteKing from "./visualAssets/whiteKing.svg";
+import blackBishop from "./visualAssets/blackBishop.svg";
+import whiteBishop from "./visualAssets/whiteBishop.svg";
+import blackKnight from "./visualAssets/blackKnight.svg";
+import whiteKnight from "./visualAssets/whiteKnight.svg";
+import blackPawn from "./visualAssets/blackPawn.svg";
+import whitePawn from "./visualAssets/whitePawn.svg";
+import blackQueen from "./visualAssets/blackQueen.svg";
+import whiteQueen from "./visualAssets/whiteQueen.svg";
+import blackRook from "./visualAssets/blackRook.svg";
+import whiteRook from "./visualAssets/whiteRook.svg";
 
+/*
+ * isCheckMate pseudocode
+ * isCheckMate(player before their turn starts)
+ *      1) iterate through edges, see if any enemies have King as destination // same as isCheck(player)
+ *          a) if not, return false
+ *          b) if so, test if King has any moves to get out of check by repeating above step
+ *             with other destination tiles
+ *              i)   if so, return false
+ *              ii)  if not, make a list S of all tiles in between King and Attacking piece
+ *                   for each attacking piece
+ *              iii) if # of attacking pieces > 1, find the intersection of S1 and S2.
+ *                   if the intersection of S1 and S2 = {}, return True. if intersection
+ *                   of S1 and S2 has elements, make S = intersection of S1 and S2
+ *              iv)  iterate through edges, if any friendlies have destination in S, return false.
+ *                   if they have no destinations in S, return True
+ *
+ * Time Complexity: O(E) where E < N(N - 1)
+ *
+ *
+ */
 
 const ChessBoard = () => {
-    return (
-        <div className="ChessBoardContainer">
-            <Tile svg={blackRook} altText="Black Rook"/>
-            <Tile svg={blackKnight} altText="Black Knight"/>
-            <Tile svg={blackBishop} altText="Black Bishop"/>
-            <Tile svg={blackQueen} altText="Black Queen"/>
-            <Tile svg={blackKing} altText="Black King"/>
-            <Tile svg={blackBishop} altText="Black Bishop"/>
-            <Tile svg={blackKnight} altText="Black Knight"/>
-            <Tile svg={blackRook} altText="Black Rook"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={blackPawn} altText="Black Pawn"/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={{}} altText=""/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whitePawn} altText="White Pawn"/>
-            <Tile svg={whiteRook} altText="White Rook"/>
-            <Tile svg={whiteKnight} altText="White Knight"/>
-            <Tile svg={whiteBishop} altText="White Bishop"/>
-            <Tile svg={whiteQueen} altText="White Queen"/>
-            <Tile svg={whiteKing} altText="White King"/>
-            <Tile svg={whiteBishop} altText="White Bishop"/>
-            <Tile svg={whiteKnight} altText="White Knight"/>
-            <Tile svg={whiteRook} altText="White Rook"/>
+  return (
+    <div>
+      {/* <div className="boardLabel">
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+      </div> */}
+      <div className="ChessBoardContainer">
+        {/* <div>
+            <p>8</p>
+        </div> */}
+        <Tile svg={blackRook} altText="Black Rook" />
+        <Tile svg={blackKnight} altText="Black Knight" />
+        <Tile svg={blackBishop} altText="Black Bishop" />
+        <Tile svg={blackQueen} altText="Black Queen" />
+        <Tile svg={blackKing} altText="Black King" />
+        <Tile svg={blackBishop} altText="Black Bishop" />
+        <Tile svg={blackKnight} altText="Black Knight" />
+        <Tile svg={blackRook} altText="Black Rook" />
+        {/* <div>
+            <p>7</p>
+        </div> */}
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        <Tile svg={blackPawn} altText="Black Pawn" />
+        {/* <div>
+            <p>6</p>
+        </div> */}
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        {/* <div>
+            <p>5</p>
+        </div> */}
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        {/* <div>
+            <p>4</p>
+        </div> */}
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        {/* <div>
+            <p>3</p>
+        </div> */}
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        <Tile svg={{}} altText="" />
+        {/* <div>
+            <p>2</p>
+        </div> */}
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        <Tile svg={whitePawn} altText="White Pawn" />
+        {/* <div>
+            <p>1</p>
+        </div> */}
+        <Tile svg={whiteRook} altText="White Rook" />
+        <Tile svg={whiteKnight} altText="White Knight" />
+        <Tile svg={whiteBishop} altText="White Bishop" />
+        <Tile svg={whiteQueen} altText="White Queen" />
+        <Tile svg={whiteKing} altText="White King" />
+        <Tile svg={whiteBishop} altText="White Bishop" />
+        <Tile svg={whiteKnight} altText="White Knight" />
+        <Tile svg={whiteRook} altText="White Rook" />
+        {/* <div />
+        <div>
+            <p>a</p>
         </div>
-    );
-}
+        <div>
+            <p>b</p>
+        </div>
+        <div>
+            <p>c</p>
+        </div>
+        <div>
+            <p>d</p>
+        </div>
+        <div>
+            <p>e</p>
+        </div>
+        <div>
+            <p>f</p>
+        </div>
+        <div>
+            <p>g</p>
+        </div>
+        <div>
+            <p>h</p>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default ChessBoard;
--- chess-v2/src/Tile.js
+++ chess-v2/src/Tile.js
@@ -4,6 +4,7 @@ const Tile = (props) => {
   return (
     <div className="Tile">
       {(props.altText !== "") && <img src={props.svg} alt={props.altText} />}
+      <span>1</span>
     </div>
   );
 };
