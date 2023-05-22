3e649e757ad9d5f634498681c005a1f10e95e077
RyanWatson425 <watsonr3@spu.edu>
2023-05-21 21:31:12 -0700

    Added authentication
    -Created basic signup/login page
    -Utilized Node.js backend to do routing
    -Leveraged StreamChat API to handle user data and create/store users
    -Hashed passwords and created .env file for security

--- chess-v2/src/App.js
+++ chess-v2/src/App.js
@@ -1,57 +1,84 @@
 import React, { Fragment, useContext } from "react";
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
 import ChessBoard from "./ChessBoard.js";
 import UserContext from "./store/user-context.js";
 import Button from "./uiComponents/Button.js";
 import { BUTTON_TYPES, STRINGS } from "./data/stringEnums.js";
 import "./App.css";
+import SignUp from "./SignUp.js";
+import Login from "./Login.js";
 
+//47:45
 const App = () => {
+  const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+  const cookies = new Cookies();
+  const token = cookies.get("token");
   const ctx = useContext(UserContext);
+  if (token) {
+    client.connectUser(
+      {
+        id: cookies.get("userId"),
+        name: cookies.get("username"),
+        firstName: cookies.get("firstName"),
+        lastName: cookies.get("lastName"),
+        hashedPassword: cookies.get("hashedPassword"),
+      },
+      token
+    ).then((user) => {
+      console.log(user);
+    });
+  }
+
   return (
-    <Fragment>
-      <header className="App-header">
-        <div className="btn-wrapper">
-          <Button
-            type={BUTTON_TYPES.PRIMARY}
-            onClick={ctx.swapPlayerColor}
-            text={STRINGS.START}
-          ></Button>
-          <div>{STRINGS.TITLE}</div>
-          <Button
-            type={BUTTON_TYPES.PRIMARY}
-            onClick={() => {}}
-            text={STRINGS.RESET}
-          ></Button>
-        </div>
-      </header>
-      <div className="App-body">
-        <div>
-          <ChessBoard />
-        </div>
-      </div>
-      <footer className="App-footer">
-        <p>
-          Developed by{" "}
-          <a
-            href="https://linkedin.com/in/ryan-watson-4a8690213"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Ryan Watson
-          </a>
-        </p>
-        <p>
-          Chess Board Piece Images from{" "}
-          <a
-            href="https://commons.wikimedia.org/wiki/Template:SVG_chess_pieces"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Wikimedia Commons
-          </a>
-        </p>
-      </footer>
-    </Fragment>
+    <div>
+      <SignUp />
+      <Login />
+    </div>
+    // <Fragment>
+    //   <header className="App-header">
+    //     <div className="btn-wrapper">
+    //       <Button
+    //         type={BUTTON_TYPES.PRIMARY}
+    //         onClick={ctx.swapPlayerColor}
+    //         text={STRINGS.START}
+    //       ></Button>
+    //       <div>{STRINGS.TITLE}</div>
+    //       <Button
+    //         type={BUTTON_TYPES.PRIMARY}
+    //         onClick={() => {}}
+    //         text={STRINGS.RESET}
+    //       ></Button>
+    //     </div>
+    //   </header>
+    //   <div className="App-body">
+    //     <div>
+    //       <ChessBoard />
+    //     </div>
+    //   </div>
+    //   <footer className="App-footer">
+    //     <p>
+    //       Developed by{" "}
+    //       <a
+    //         href="https://linkedin.com/in/ryan-watson-4a8690213"
+    //         target="_blank"
+    //         rel="noreferrer"
+    //       >
+    //         Ryan Watson
+    //       </a>
+    //     </p>
+    //     <p>
+    //       Chess Board Piece Images from{" "}
+    //       <a
+    //         href="https://commons.wikimedia.org/wiki/Template:SVG_chess_pieces"
+    //         target="_blank"
+    //         rel="noreferrer"
+    //       >
+    //         Wikimedia Commons
+    //       </a>
+    //     </p>
+    //   </footer>
+    // </Fragment>
   );
 };
 
---
+++ chess-v2/src/Login.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Axios from "axios";
+import Cookies from "universal-cookie";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const cookies = new Cookies();
+  const login = () => {
+    Axios.post("http://localhost:3001/login", { username, password }).then(
+      (res) => {
+        const { token, userId, firstName, lastName, username } =
+          res.data;
+        cookies.set("token", token);
+        cookies.set("userId", userId);
+        cookies.set("firstName", firstName);
+        cookies.set("lastName", lastName);
+        cookies.set("username", username);
+      }
+    );
+  };
+  return (
+    <div>
+      <label>Login</label>
+      <input
+        placeholder="Username"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
